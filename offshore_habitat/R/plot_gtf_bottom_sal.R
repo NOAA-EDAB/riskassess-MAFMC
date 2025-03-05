f3ccb5290112838264c599ac023526ffee94eceb
Maxwell Grezlik <maxwell.grezlik@noaa.gov>
2025-03-05 13:19:08 -0500

    added trend line to monthly weighted mean bottom salinity plot

--- offshore_habitat/R/plot_gtf_bottom_sal.R
+++ offshore_habitat/R/plot_gtf_bottom_sal.R
@@ -30,30 +30,44 @@ plot_gtf_sal <- function(shadedRegion = NULL,
   
   load(file = here::here('offshore_habitat/data/gtf_bottom_sal.rda'))
   
+  # Changing Time from decimal year to integer of months since
+  # the start of the time series so the geom_gls function can be used.
+  # Year is retained for labeling purposes.
+  test <- gtf_bottom_sal |> 
+            dplyr::filter(Var == 'weighted_mean_sal_78m') |>
+            dplyr::mutate(Year = Time) |> 
+            dplyr::mutate(Time = 1:length(Var))
+  
+  # Create table for X axis label
+  lab.data <- test |>
+                dplyr::filter(Year == floor(Year)) |> 
+                dplyr::filter(Year %in% c(2000,2010,2020))
+  
+  # Create ribbon data for confidence interval
   ribbon<- gtf_bottom_sal |>
-    tidyr::pivot_wider(names_from = Var, values_from = Value)
+    tidyr::pivot_wider(names_from = Var, values_from = Value) |> 
+    dplyr::mutate(Time = 1:length(Units))
   
   # code for generating plot object p
   # ensure that setup list objects are called as setup$...
   # e.g. fill = setup$shade.fill, alpha = setup$shade.alpha,
   # xmin = setup$x.shade.min , xmax = setup$x.shade.max
   #
-  p <- gtf_bottom_sal |>
-    dplyr::filter(Var == 'weighted_mean_sal_78m') |>
+  p <- test |>
     ggplot2::ggplot() +
-    #Highlight last ten years
-    ggplot2::annotate("rect", fill = setup$shade.fill, alpha = setup$shade.alpha,
-                      xmin = setup$x.shade.min , xmax = setup$x.shade.max,
-                      ymin = -Inf, ymax = Inf) +
     ggplot2::geom_line(ggplot2::aes(x = Time, y = Value))+
     ggplot2::geom_point(ggplot2::aes(x = Time, y = Value))+
+    ggplot2::scale_x_continuous(
+      breaks = lab.data$Time,    
+      labels = lab.data$Year
+      ) +
     ggplot2::geom_hline(yintercept=33,linetype=setup$hline.lty)+
     ggplot2::geom_hline(yintercept=36,linetype=setup$hline.lty)+
     ggplot2::geom_ribbon(data = ribbon, ggplot2::aes(ymin = conf.low , ymax = conf.high , x = Time), alpha = 0.2)+
     ggplot2::ggtitle("Weighted mean salinity")+
     ggplot2::ylab("Degree C")+
     ggplot2::xlab(ggplot2::element_blank())+
-    # ecodata::geom_gls(ggplot2::aes(x = Time, y = Value)) +
+    ecodata::geom_gls(ggplot2::aes(x = Time, y = Value)) +
     ecodata::theme_ts()+
     ecodata::theme_facet()+
     ecodata::theme_title()
